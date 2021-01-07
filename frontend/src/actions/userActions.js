d4c44cfc51876e7e859e2e92dab1277f3fc3b0a7
Rezan92 <rezanibrahim8@gmail.com>
2021-01-07 10:29:35 +0100

    User Login Reducer & Action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,35 @@
+import * as actions from "../constants/userConstants";
+import axios from "axios";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: actions.USER_LOGIN_REQUEST,
+    });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: actions.USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: actions.USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
