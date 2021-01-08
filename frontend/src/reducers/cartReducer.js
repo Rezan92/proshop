e6aa3616b4f8a29bc64fd60bca43842fa9039a23
Rezan92 <rezanibrahim8@gmail.com>
2021-01-08 21:48:26 +0100

    Shipping Screen & Save Address

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,6 +1,9 @@
 import * as actions from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case actions.CART_ADD_ITEM:
       const item = action.payload;
@@ -24,6 +27,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+    case actions.CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
       };
     default:
       return state;
