37ec00ce15aa7a0f181e1d771e9cadf81289f5f7
Rezan92 <rezanibrahim8@gmail.com>
2021-01-09 18:06:35 +0100

    Order Pay Reducer & Action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -46,3 +46,26 @@ export const orderDetailsReducer = (
       return state;
   }
 };
+
+export const orderPayReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actions.ORDER_PAY_REQUEST:
+      return {
+        loading: true,
+      };
+    case actions.ORDER_PAY_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case actions.ORDER_PAY_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case actions.ORDER_PAY_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
