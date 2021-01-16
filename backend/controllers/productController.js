dfd6e4a73ce83d296d9b61df69a6c274e0b88bdc
Rezan92 <rezanibrahim8@gmail.com>
2021-01-16 17:01:46 +0100

    Product Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,9 @@ import asyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -11,8 +14,13 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+  const count = await Product.countDocuments({ ...keyword });
+
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 const getProductsById = asyncHandler(async (req, res) => {
