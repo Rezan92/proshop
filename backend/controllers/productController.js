30172cdccb8b1612b076dc41958ae5b46b62ede7
Rezan92 <rezanibrahim8@gmail.com>
2021-01-16 17:44:18 +0100

    Top Products Carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -130,6 +130,12 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductsById,
@@ -137,4 +143,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
